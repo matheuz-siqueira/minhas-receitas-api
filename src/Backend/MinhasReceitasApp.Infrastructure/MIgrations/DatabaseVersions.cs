ff475f0a0a795e60628c538f6c006d46d8b3c5fb
matheuz-siqueira <matheusbatista.dev@gmail.com>
2025-04-18 11:45:47 -0300

    feat: creating schema via api

---
+++ src/Backend/MinhasReceitasApp.Infrastructure/MIgrations/DatabaseVersions.cs
@@ -0,0 +1,6 @@
+namespace MinhasReceitasApp.Infrastructure.MIgrations;
+
+public abstract class DatabaseVersions
+{
+    public const int TABLE_USER = 1; 
+}
---
+++ src/Backend/MinhasReceitasApp.Infrastructure/MIgrations/Versions/Version0000001.cs
@@ -0,0 +1,15 @@
+using FluentMigrator;
+
+namespace MinhasReceitasApp.Infrastructure.MIgrations.Versions;
+
+[Migration(DatabaseVersions.TABLE_USER, "Create table to save the user's information")]
+public class Version0000001 : VersionBase
+{
+    public override void Up()
+    {
+        Create.Table("Users")
+            .WithColumn("Name").AsString(100).NotNullable()
+            .WithColumn("Email").AsString(255).NotNullable()
+            .WithColumn("Password").AsString(2000).NotNullable();
+    }
+}
