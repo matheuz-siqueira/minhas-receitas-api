b99958c192aea321baa7860a18e063f157f81858
matheuz-siqueira <matheusbatista.dev@gmail.com>
2025-04-26 20:27:48 -0300

    feat: adding unit test for get profile usecase

---
+++ tests/CommonTestUtilities/LoggedUser/LoggedUserBuilder.cs
@@ -0,0 +1,16 @@
+using MinhasReceitasApp.Domain.Entities;
+using MinhasReceitasApp.Domain.Services.LoggedUser;
+using Moq;
+
+namespace CommonTestUtilities.LoggedUser;
+
+public class LoggedUserBuilder
+{
+    public static ILoggedUser Build(User user)
+    {
+        var mock = new Mock<ILoggedUser>(); 
+        mock.Setup(x => x.User()).ReturnsAsync(user); 
+
+        return mock.Object; 
+    }
+}
